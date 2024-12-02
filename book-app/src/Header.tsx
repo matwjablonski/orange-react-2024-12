6f4f3fe184136b9020b678f58cf1eeb0d80e3b9a
Mateusz Jabłoński <mail@mateuszjablonski.com>
2024-12-02 12:40:05 +0100

    task 3

--- book-app/src/Header.tsx
+++ book-app/src/Header.tsx
@@ -1,3 +1,16 @@
-export const Header = () => (
-  <header>Witaj w naszej aplikacji książkowej</header>
-);
+import { useState } from 'react';
+
+export const Header = () => {
+  const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    setIsUserLoggedIn((prevState) => !prevState);
+  };
+
+  return (
+    <header>
+      <p>Witaj w naszej aplikacji książkowej {isUserLoggedIn ? 'Mateuszu' : ''}</p>
+      <button type="button" onClick={handleLogin}>{isUserLoggedIn ? 'Wyloguj' : 'Zaloguj'}</button>
+    </header>
+  )
+};
