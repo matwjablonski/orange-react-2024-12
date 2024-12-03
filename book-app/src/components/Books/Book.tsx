251a99ca3a0d94cf8e27d1c3ccd98c238029678e
Mateusz Jabłoński <mail@mateuszjablonski.com>
2024-12-03 11:39:03 +0100

    state with default value as func

--- book-app/src/components/Books/Book.tsx
+++ book-app/src/components/Books/Book.tsx
@@ -5,19 +5,26 @@ import { BookProps } from './types';
 
 export const Book = ({ title, author, publicationDate }: BookProps) => {
   const [isRead, setIsRead] = useState(false);
+  const [votes, setVotes] = useState<number>(() => Math.floor(author.length * 2 / 5));
 
   const handleRead = () => {
     setIsRead((prevState) => !prevState);
   };
+
+  const handleUpdateVotes = () => {
+    setVotes((prevState) => prevState + 1);
+  }
 
   const buttonStyles = { backgroundColor: 'beige', color: 'darkblue' };
 
   return (
     <li>
       <div>
+        {votes > 0 && <div>Liczba głosów: {votes}</div>}
         { isRead && <div><em>Przeczytana</em></div> }
         {title} by {author} ({publicationDate as string})
         <button type="button" style={buttonStyles} onClick={handleRead}>{isRead ? 'Usuń z przeczytanych' : 'Dodaj do przeczytanych'}</button>
+        <button type="button" onClick={handleUpdateVotes}>Głosuj na tę pozycję</button>      
       </div>
     </li>
   )
